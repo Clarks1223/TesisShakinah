e8390777bd9afb80bf65d9bc85a1326b17c9f538
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-01-16 00:29:23 -0500

    Solo falta verificar la cuenta en el correo

--- src/components/AgendarCita/ItemCita.jsx
+++ src/components/AgendarCita/ItemCita.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ItemCita.css";
 import { useCita } from "../../Context/CitaContext";
 import { useForm } from "react-hook-form";
 import { useAuth } from "../../Context/AuthContext";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { fireStore } from "../../Auth/firebase";
 
-const ItemCita = (props) => {
-  const { userId } = useAuth();
+const ItemCita = ({ nombreEmpleado, EmpleadoID, foto, titulo, precio, id }) => {
+  const { userId, verItem } = useAuth();
   const { eliminarCita, agendarCitaBase } = useCita();
 
   const {
@@ -16,46 +18,130 @@ const ItemCita = (props) => {
 
   const getCurrentDate = () => {
     const today = new Date();
-    const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    const fechaFormateada = today.toLocaleDateString("es-ES", options);
-    return fechaFormateada;
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
   };
 
-  const getCurrentTime = () => {
-    const now = new Date();
-    return now.toTimeString().slice(0, 5);
+  const generateHourOptions = () => {
+    const startHour = 8;
+    const endHour = 17;
+    const minutesInterval = 30;
+    const options = [];
+    for (let hour = startHour; hour <= endHour; hour++) {
+      for (let minute = 0; minute < 60; minute += minutesInterval) {
+        const formattedHour = String(hour).padStart(2, "0");
+        const formattedMinute = String(minute).padStart(2, "0");
+        const time = `${formattedHour}:${formattedMinute}`;
+        options.push(
+          <option key={time} value={time}>
+            {time}
+          </option>
+        );
+      }
+    }
+    return options;
   };
 
-  const onSubmit = (data) => {
-    data.Empleado = props.nombreEmpleado;
-    data.IDEmpleado = props.EmpleadoID;
+  const [disabledDays, setDisabledDays] = useState([6]);
+
+  const verificarCitasHora = async (idEmpleado, fecha, hora, estado) => {
+    try {
+      const collectionRef = collection(fireStore, "Citas");
+      const q = query(
+        collectionRef,
+        where("IDEmpleado", "==", idEmpleado),
+        where("Fecha", "==", fecha),
+        where("Hora", "==", hora),
+        where("Estado", "==", estado)
+      );
+      const querySnapshot = await getDocs(q);
+      return querySnapshot.size > 0;
+    } catch (error) {
+      console.error("Error al verificar citas:", error);
+      return true;
+    }
+  };
+  const empleadoHorario = async (id) => {
+    try {
+      const usuario = await verItem("Personal", id);
+      console.log("Se ha recuperado este usuario:", usuario.dias_no_laborables);
+      setDisabledDays(usuario.dias_no_laborables);
+    } catch (error) {
+      console.error("Error al recuperar el usuario:", error);
+    }
+  };
+
+  const generarMensajeDiasDeshabilitados = (diasDeshabilitados) => {
+    const diasSemana = [
+      "Lunes",
+      "Martes",
+      "Miércoles",
+      "Jueves",
+      "Viernes",
+      "Sábado",
+      "Domingo",
+    ];
+    return `El empleado ha deshabilitado los día${
+      diasDeshabilitados.length > 1 ? "s" : ""
+    } ${diasDeshabilitados.map((dia) => diasSemana[dia]).join(" y ")}`;
+  };
+
+  const onSubmit = async (data) => {
+    data.Empleado = nombreEmpleado;
+    data.IDEmpleado = EmpleadoID;
     data.IDUsuario = userId;
-    data.IDservicio = props.id;
-    data.Foto = props.foto;
-    data.Titulo = props.titulo;
-    data.Precio = props.precio;
+    data.IDservicio = id;
+    data.Foto = foto;
+    data.Titulo = titulo;
+    data.Precio = precio;
     data.Estado = "Activo";
-    
-    agendarCitaBase(data, props.id);
+
+    const fechaSeleccionada = new Date(data.Fecha);
+    const diaSeleccionado = fechaSeleccionada.getDay();
+
+    await empleadoHorario(EmpleadoID);
+    console.log("Se ha actualizado los días no laborables", disabledDays);
+
+    if (disabledDays.includes(diaSeleccionado)) {
+      const mensajeDiasDeshabilitados =
+        generarMensajeDiasDeshabilitados(disabledDays);
+      alert(mensajeDiasDeshabilitados);
+      return;
+    }
+
+    const hayConflicto = await verificarCitasHora(
+      EmpleadoID,
+      data.Fecha,
+      data.Hora,
+      "Activo"
+    );
+
+    if (!hayConflicto) {
+      console.log("Listo para agendar cita");
+      console.log("data: ", data);
+      agendarCitaBase(data, id);
+    } else {
+      alert(
+        "Este empleado ya cuenta con una cita en este horario, selecciona otro horario."
+      );
+      console.log("Existe un conflicto de horario, selecciona otro horario.");
+    }
   };
 
   return (
     <section className="contendor-agendar-cita">
       <div className="nombre-empleado">
-        <h3>{props.nombreEmpleado}</h3>
+        <h3>{nombreEmpleado}</h3>
       </div>
       <div className="agendar-cita-imagen">
-        <img src={props.foto} alt={props.nombreEmpleado} />
+        <img src={foto} alt={nombreEmpleado} />
       </div>
 
       <div className="agendar-cita-detalle">
-        <h2>{props.titulo}</h2>
-        <h2>{`${props.precio}$`}</h2>
+        <h2>{titulo}</h2>
+        <h2>{`${precio}$`}</h2>
       </div>
 
       <form className="agendar-cita-datos" onSubmit={handleSubmit(onSubmit)}>
@@ -72,25 +158,16 @@ const ItemCita = (props) => {
         </div>
         <div className="select-hora">
           <label>Hora:</label>
-          <input
-            type="time"
-            {...register("Hora", {
-              required: true,
-              validate: (value) => {
-                const selectedTime = new Date(`1970-01-01T${value}`);
-                return (
-                  selectedTime >= new Date(`1970-01-01T08:00`) &&
-                  selectedTime <= new Date(`1970-01-01T17:00`)
-                );
-              },
-            })}
-          />
+          <select {...register("Hora", { required: true })}>
+            {generateHourOptions()}
+          </select>
           {errors.Hora && (
             <span className="error-message">
               Selecciona una hora entre las 8 am y las 5 pm
             </span>
           )}
         </div>
+
         <button type="submit" className="agendar-cita-boton">
           Agendar
         </button>
@@ -99,7 +176,7 @@ const ItemCita = (props) => {
       <div>
         <button
           className="eliminar-cita-boton"
-          onClick={() => eliminarCita(props.id)}
+          onClick={() => eliminarCita(id)}
         >
           Eliminar
         </button>
