15d791bd48f82bd059b8095aeaf3baf48705d6e4
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-01-17 11:26:42 -0500

    Navegacion corregida

--- src/components/Home/ServiciosComponent/Item/ServiciosItem.jsx
+++ src/components/Home/ServiciosComponent/Item/ServiciosItem.jsx
@@ -1,10 +1,21 @@
-import "./ServiciosItem.css";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useCita } from "../../../../Context/CitaContext";
-import { Link } from "react-router-dom";
+import "./ServiciosItem.css";
+import { useAuth } from "../../../../Context/AuthContext";
+
 export const ServiciosItem = (props) => {
   const { agregarCita, cita } = useCita();
+  const { user } = useAuth(); // Obtén la información de autenticación
+  const navigate = useNavigate();
 
   const handleAgregarCita = () => {
+    if (!user) {
+      // Si el usuario no ha iniciado sesión, puedes mostrar un mensaje o redirigir a la página de inicio de sesión
+      alert("Debes iniciar sesión para agendar una cita.");
+      // Puedes redirigir al usuario a la página de inicio de sesión si lo prefieres
+      return navigate("/Login");
+    }
     const nuevaCita = {
       id: props.id,
       titulo: props.titulo,
@@ -13,30 +24,31 @@ export const ServiciosItem = (props) => {
       nombreEmpleado: props.nombreEmpleado,
       EmpleadoID: props.EmpleadoID,
     };
-    // Verificar si ya existe una cita con el mismo ID
+
     const citaExistente = cita.find((c) => c.id === nuevaCita.id);
     if (!citaExistente) {
       agregarCita(nuevaCita);
     } else {
       console.log("Ya existe una cita con el mismo ID.");
     }
+    return navigate("/Usuario/Agendar");
   };
 
   return (
     <section className="ServiciosOfertados-section">
       <section className="ServiciosOfertados-section-imagen">
-        <img src={props.foto} />
+        <img src={props.foto} alt={props.titulo} />
       </section>
       <section className="ServiciosOfertados-descripcion">
         <p>{props.titulo}</p>
         <p>{`${props.precio}$`}</p>
-        <Link to="/Usuario/Agendar">
-          <button className="boton-agregar-cita" onClick={handleAgregarCita}>
-            Agendar Cita
-          </button>
-        </Link>
+
+        <button className="boton-agregar-cita" onClick={handleAgregarCita}>
+          Agendar Cita
+        </button>
       </section>
     </section>
   );
 };
+
 export default ServiciosItem;
--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,41 +1,30 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
 import { fireBaseApp } from "../Auth/firebase";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
 
 const auth = getAuth(fireBaseApp);
 
 const ProtectedRoute = ({ redirectPath = "/Login" }) => {
-  const [user, setUser] = useState(null);
-  const [mailVerified, setMailVerified] = useState(false);
+  const [mailVerified, setMailVerified] = useState(auth.currentUser?.emailVerified);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-
-      if (user) {
-        const checkEmailVerification = async () => {
-          const updatedUser = await auth.currentUser.reload();
-          setMailVerified(updatedUser.emailVerified);
-        };
-
-        checkEmailVerification();
+    const checkEmailVerification = async () => {
+      if (auth.currentUser) {
+        const updatedUser = await auth.currentUser.reload();
+        setMailVerified(updatedUser.emailVerified);
       }
-    });
+    };
+
+    checkEmailVerification();
 
     return () => {
-      unsubscribe();
+      // Limpiar efecto al desmontar el componente si es necesario
     };
-  }, []);
-
-  if (!user) {
-    // No hay usuario autenticado, redirige al login
-    return <Navigate to={redirectPath} replace />;
-  }
+  }, [auth.currentUser]);
 
   if (mailVerified === false) {
-    // El correo no ha sido verificado, alerta y redirige
     alert("Verifique su cuenta de correo");
     return <Navigate to={redirectPath} replace />;
   }
